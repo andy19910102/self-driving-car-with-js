3e5225a2eb8a8ca19dbd2e9b81de4b4ae38ea95f
An Sheng Huang <anshenghuang@AndeMacBook-Pro.local>
2022-06-19 22:09:03 +0800

    Define the road

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9)
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 
 animate();
@@ -14,7 +15,15 @@ function animate() {
     car.update();
     // Set canvas height to 100vh
     canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height * 0.7);
+
+    road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
+
     // The window.requestAnimationFrame() method tells 
     // the browser that you wish to perform an animation 
     // and requests that the browser calls a specified 
